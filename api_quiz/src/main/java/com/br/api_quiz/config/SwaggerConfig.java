95b6dec54cec058c6c9e617d608d3a241c1a3829
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-29 14:06:37 -0300

    Adição de rota para o swagger.

--- api_quiz/src/main/java/com/br/api_quiz/config/SwaggerConfig.java
+++ api_quiz/src/main/java/com/br/api_quiz/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
     public Docket getDocket(){
         return new Docket(DocumentationType.SWAGGER_2)
         .select()
-        .apis(RequestHandlerSelectors.basePackage("basePackage"))
+        .apis(RequestHandlerSelectors.basePackage("com.br.api_quiz.controllers"))
         .build()
         .apiInfo(metaData());
     }
