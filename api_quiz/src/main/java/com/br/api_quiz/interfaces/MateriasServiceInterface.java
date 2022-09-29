7e4e928fb1a4d1496375afb0ecd900cc447d4b37
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-29 13:45:29 -0300

    Adição de metodos.

--- api_quiz/src/main/java/com/br/api_quiz/interfaces/MateriasServiceInterface.java
+++ api_quiz/src/main/java/com/br/api_quiz/interfaces/MateriasServiceInterface.java
@@ -1,5 +1,13 @@
 package com.br.api_quiz.interfaces;
 
+import java.util.List;
+
+import com.br.api_quiz.models.MateriasModel;
+
 public interface MateriasServiceInterface {
     
+
+    public List<MateriasModel> findAll();
+
+    public MateriasModel findId(Integer id);
 }
