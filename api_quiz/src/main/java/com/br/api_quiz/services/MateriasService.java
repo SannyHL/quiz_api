49430739d7020aa71b93b6e0b48ac1a1ead0de40
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-29 13:18:37 -0300

    Criação de Service.

---
+++ api_quiz/src/main/java/com/br/api_quiz/services/MateriasService.java
@@ -0,0 +1,11 @@
+package com.br.api_quiz.services;
+
+import org.springframework.stereotype.Service;
+
+import com.br.api_quiz.interfaces.MateriasServiceInterface;
+
+@Service
+public class MateriasService implements MateriasServiceInterface{
+    
+    
+}
