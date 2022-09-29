55735b87b2be9107b4b97e677c1f36db541c03a8
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-29 13:17:02 -0300

    Criação Service do ModelMapper.

---
+++ api_quiz/src/main/java/com/br/api_quiz/services/ModelMapperService.java
@@ -0,0 +1,15 @@
+package com.br.api_quiz.services;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ModelMapperService {
+
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
+    
+}
