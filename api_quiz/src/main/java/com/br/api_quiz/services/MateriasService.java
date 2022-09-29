3bf9913a1fea4a2b930e2b662368046ae628854e
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-29 13:46:07 -0300

    Criação dos métodos findAll e findById.

--- api_quiz/src/main/java/com/br/api_quiz/services/MateriasService.java
+++ api_quiz/src/main/java/com/br/api_quiz/services/MateriasService.java
@@ -1,11 +1,35 @@
 package com.br.api_quiz.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.br.api_quiz.interfaces.MateriasServiceInterface;
+import com.br.api_quiz.models.MateriasModel;
+import com.br.api_quiz.repositories.MateriasRepository;
 
 @Service
 public class MateriasService implements MateriasServiceInterface{
+
+    @Autowired
+    public MateriasRepository repository;
+    @Autowired
+    public ModelMapper mapper;
+
+    @Override
+    public List<MateriasModel> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public MateriasModel findId(Integer id) {
+        Optional<MateriasModel> materiasOptional = repository.findById(id);
+        return materiasOptional.get();
+    }
     
-    
+   
+
 }
