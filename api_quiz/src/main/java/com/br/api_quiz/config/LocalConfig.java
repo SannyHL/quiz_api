d29589e683b9ecb21640f956b51d658a608bd844
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-29 13:59:52 -0300

    Criação de profile local.

---
+++ api_quiz/src/main/java/com/br/api_quiz/config/LocalConfig.java
@@ -0,0 +1,31 @@
+package com.br.api_quiz.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.br.api_quiz.enums.MateriasEnum;
+import com.br.api_quiz.models.MateriasModel;
+import com.br.api_quiz.repositories.MateriasRepository;
+
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+    @Autowired
+    private MateriasRepository repository;
+
+    @Bean
+    public void startDataBase(){
+        var questao1 = new MateriasModel(null, MateriasEnum.MATEMATICA, "Qual o resultado de 5x10?", "50", "55", "25", "60");
+
+        var questao2 = new MateriasModel(null, MateriasEnum.MATEMATICA, "Qual o resultado de 6x10?", "60", "75", "25", "50");
+
+        repository.saveAll(List.of(questao1, questao2));
+    }
+    
+}
