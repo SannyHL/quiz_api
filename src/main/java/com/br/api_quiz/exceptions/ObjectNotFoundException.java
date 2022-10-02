97eb6c3dec76b394aee47f5e8f61611820d6e3cd
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-02 18:43:41 -0300

    Criação de exception.

---
+++ src/main/java/com/br/api_quiz/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.br.api_quiz.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+    
+}
