156dc298b65f7e2efaaf572b208429b28571e523
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-02 18:43:23 -0300

    Criação do método findById.

--- src/main/java/com/br/api_quiz/controllers/MateriasController.java
+++ src/main/java/com/br/api_quiz/controllers/MateriasController.java
@@ -7,6 +7,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +30,10 @@ public class MateriasController {
         .stream().map(objetos -> mapper.map(objetos, MateriasDTO.class))
         .collect(Collectors.toList()));
     }
-    
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MateriasDTO> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(mapper.map(service.findId(id), MateriasDTO.class)); 
+    }
+       
 }
