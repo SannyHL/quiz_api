2859f79ea7e0c0e55df32500c639ab5956158fba
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-03 16:30:26 -0300

    Criação de método findByMateria.

--- src/main/java/com/br/api_quiz/repositories/MateriasRepository.java
+++ src/main/java/com/br/api_quiz/repositories/MateriasRepository.java
@@ -1,9 +1,12 @@
 package com.br.api_quiz.repositories;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.br.api_quiz.enums.MateriasEnum;
 import com.br.api_quiz.models.MateriasModel;
 
 public interface MateriasRepository extends JpaRepository<MateriasModel, Integer>{
     
+    List<MateriasModel> findByMateria(MateriasEnum materia);
 }
