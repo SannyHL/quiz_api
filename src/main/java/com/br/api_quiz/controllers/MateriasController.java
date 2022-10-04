39ff4c3bb9cf8375670f743324ce5e836e89dbd7
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-04 17:29:21 -0300

    Inserção delete.

--- src/main/java/com/br/api_quiz/controllers/MateriasController.java
+++ src/main/java/com/br/api_quiz/controllers/MateriasController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -72,6 +73,10 @@ public class MateriasController {
         .update(materiasDTO), MateriasDTO.class));
     }
 
-    
-       
+    @DeleteMapping(ID)
+    public ResponseEntity<MateriasDTO> delete(@PathVariable Integer id){
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+  
 }
