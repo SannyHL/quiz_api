eaf21113002ca76f7ed0165c9eefa42e22dc1f87
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-05 01:20:49 -0300

    Criação de classe para teste de service.

---
+++ src/test/java/com/br/api_quiz/services/QuestionsServiceTest.java
@@ -0,0 +1,35 @@
+package com.br.api_quiz.services;
+
+import org.junit.jupiter.api.Test;
+
+public class QuestionsServiceTest {
+    @Test
+    void testCreate() {
+
+    }
+
+    @Test
+    void testDeleteById() {
+
+    }
+
+    @Test
+    void testFindAll() {
+
+    }
+
+    @Test
+    void testFindByMateria() {
+
+    }
+
+    @Test
+    void testFindId() {
+
+    }
+
+    @Test
+    void testUpdate() {
+
+    }
+}
